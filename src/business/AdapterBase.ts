72913d1fd22546bd460eb36b97be2bec7ac67e55
wsa <984292420>
2020-05-17 09:17:14 +0800

    feat: 去除 lite-server 安全，以及添加 测试目录

---
+++ src/business/AdapterBase.ts
@@ -0,0 +1,7 @@
+export interface IAdapter<T> {
+	add(t: T) : void
+	remove() : T | undefined
+	clear(): void
+	length: number
+	isEmpty: boolean
+}
--- src/business/LinkedList.ts
+++ src/business/LinkedList.ts
@@ -1,3 +1,53 @@
 export class ListNode<T> {
+	public next: ListNode<T> | null;
+	public prev: ListNode<T> | null
+	public data: T | undefined
+	public constructor(data: T | undefined = void 0) {
+		this.next = this.prev = null
+		this.data = data
+	}
+}
 
+
+export class LinkedList<T> {
+	private _headNode: ListNode<T>
+	private _length: number
+
+	public constructor() {
+		this._headNode = new ListNode<T>()
+		this._headNode.next = this._headNode
+		this._headNode.prev = this._headNode
+		this._length = 0
+	}
+
+	public empty(): boolean {
+		return this._headNode.next === this._headNode
+	}
+
+	public get length(): number {
+		return this._length
+	}
+
+	public begin(): ListNode<T> {
+		if(this._headNode.next === null) {
+			throw new Error('头节点之争为null')
+		}
+
+		return this._headNode.next
+	}
+
+	public end() {
+		return this._headNode
+	}
+
+	public contains(data: T): boolean {
+		for (let link: ListNode<T> | null = this._headNode.next;
+		     link !== null && link != this._headNode;
+		     link = (link as ListNode<T>).next) {
+			if (link !== null) {
+				return link.data !== void 0 && data === link.data
+			}
+		}
+		return false
+	}
 }

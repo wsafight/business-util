c32805ca12680b6786ba49342b211ed92658d7c9
王世安 <984292420@qq.com>
2020-09-24 00:24:51 +0800

    feat: 为转换颜色添加 rgb 颜色

--- src/fun/contrast-text-color.ts
+++ src/fun/contrast-text-color.ts
@@ -3,22 +3,38 @@
 
 const colorByBgColor = new Map()
 
+const rgbRegex = /^rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/
+
+const hex = (x: string) => ("0" + parseInt(x).toString(16)).slice(-2);
+
+function convertRGBToHex(rgb: string): string {
+  const bg = rgb.match(rgbRegex);
+  if (!bg) {
+    return ''
+  }
+  return ("#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3])).toUpperCase();
+}
+
 /**
- * 根据当前的背景颜色确认文本颜色
- * @param backgroundHexColor
+ * 根据当前的背景颜色确认适合背景色的文本颜色 (文本仅仅支持黑 和 白)
+ * @param backgroundColor 字符串  #FFFBBC | rgb(222,33,44) 均可
  */
-export function contrastTextColor(backgroundHexColor: string) {
+export function contrastTextColor(backgroundColor: string) {
+  //
+  const backgroundHexColor = backgroundColor.length > 7 ? convertRGBToHex(backgroundColor) : backgroundColor
+
   if (colorByBgColor.has(backgroundHexColor)) {
     return colorByBgColor.get(backgroundHexColor)
   }
 
   let hex = backgroundHexColor
-  if (hex.indexOf('#') === 0) {
+  if (hex.startsWith('#')) {
     hex = hex.substring(1);
   }
   if (hex.length === 3) {
-    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+    hex = [hex[0], hex[0], hex[1], hex[1], hex[2], hex[2]].join('')
   }
+
   if (hex.length !== 6) {
     throw new Error('Invalid hex color.' + backgroundHexColor);
   }

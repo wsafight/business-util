0721f5db1c64b9961bf3004f4c6fbc4c4013c498
王世安 <984292420@qq.com>
2020-09-24 14:21:49 +0800

    feat: 修改下载注释

--- src/business/download-file.ts
+++ src/business/download-file.ts
@@ -39,17 +39,23 @@ function downloadContent(data:  string | ArrayBuffer | ArrayBufferView | Blob,
                          filename: string,
                          mime?: string,
                          bom?: string) {
-  const blobData = bom ? [bom, data] : [data]
+
+  // 使用 typeof 避免 IE 11 bug
+  const blobData = (typeof bom !== 'undefined') ? [bom, data] : [data]
   const blob = new Blob(blobData, {type: mime || 'application/octet-stream'});
+
+  // IE9 以上的 IE 浏览器都会报一个 request URI too large 的错误, 直接使用 msSaveBlob
   if (typeof window.navigator.msSaveBlob !== 'undefined') {
     window.navigator.msSaveBlob(blob, filename);
   } else {
+
     const blobURL = (window.URL && window.URL.createObjectURL) ? window.URL.createObjectURL(blob) : window.webkitURL.createObjectURL(blob);
     const tempLink = document.createElement('a');
     tempLink.style.display = 'none';
     tempLink.href = blobURL;
     tempLink.setAttribute('download', filename);
 
+    // 当前浏览器不支持 h5 download，打开
     if (typeof tempLink.download === 'undefined') {
       tempLink.setAttribute('target', '_blank');
     }
@@ -57,6 +63,7 @@ function downloadContent(data:  string | ArrayBuffer | ArrayBufferView | Blob,
     document.body.appendChild(tempLink);
     tempLink.click();
 
+    // 修复移动端 safari 浏览器下载 bug "webkit blob resource error 1"
     setTimeout(function () {
       document.body.removeChild(tempLink);
       window.URL.revokeObjectURL(blobURL);

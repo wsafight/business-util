a66e39cace238b53c2f6e3f511676fa6765af164
王世安 <984292420@qq.com>
2020-11-18 22:48:34 +0800

    feat: Promise 状态完善

--- src/business/MyPromise.ts
+++ src/business/MyPromise.ts
@@ -1,8 +1,41 @@
+type PromiseState = 'pending' | 'fulfilled' | 'rejected'
+
 class MyPromise {
-  constructor(executor: any) {
+  private state: PromiseState = 'pending'
+  private value: any = null;
+  private reason: any = null
+
+  private resolve(value: any) {
+    if (this.state === 'pending') {
+      this.value = value
+      this.state = 'fulfilled'
+    }
   }
 
-  then(onfulfilled: any, onrejected: any) {
+  private reject(reason: any) {
+    if (this.state === 'pending') {
+      this.reason = reason
+      this.state = 'rejected'
+    }
+  }
+
+  constructor(executor: any) {
+    executor(this.resolve, this.reject)
+  }
+
+
+
+  then(onFulfilled: any, onRejected: any) {
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (data: any) => data
+    onRejected = typeof onRejected === 'function' ? onRejected : (error: any) => {
+      throw error
+    }
+    if (this.state === 'fulfilled') {
+      onFulfilled(this.value)
+    }
+    if (this.state === 'rejected') {
+      onRejected(this.reason)
+    }
 
   }
 }

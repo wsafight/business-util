970855de5b1e1ea9dc4e2c2e956322b6a4f23e8a
王世安 <984292420@qq.com>
2020-11-18 23:28:02 +0800

    feat: 后面修改

--- src/business/MyPromise.ts
+++ src/business/MyPromise.ts
@@ -2,7 +2,43 @@ type PromiseState = 'pending' | 'fulfilled' | 'rejected'
 
 /**
  * 微任务
+ *
+ * 更新一下，现在 Vue 的 nextTick 实现移除了 MutationObserver 的方式（兼容性原因），取而代之的是使用 MessageChannel。
  */
+
+//  termine (macro) task defer implementation.
+// // Technically setImmediate should be the ideal choice, but it's only available
+// // in IE. The only polyfill that consistently queues the callback after all DOM
+// // events triggered in the same loop is by using MessageChannel.
+// /* istanbul ignore if */
+// // 如果浏览器不支持Promise，使用宏任务来执行nextTick回调函数队列
+// // 能力检测，测试浏览器是否支持原生的setImmediate（setImmediate只在IE中有效）
+// if (typeof setImmediate !== 'undefined' && isNative(setImmediate)) {
+//   // 如果支持，宏任务（ macro task）使用setImmediate
+//   macroTimerFunc = () => {
+//     setImmediate(flushCallbacks)
+//   }
+//   // 同上
+// } else if (typeof MessageChannel !== 'undefined' && (
+//   isNative(MessageChannel) ||
+//   // PhantomJS
+//   MessageChannel.toString() === '[object MessageChannelConstructor]'
+// )) {
+//   const channel = new MessageChannel()
+//   const port = channel.port2
+//   channel.port1.onmessage = flushCallbacks
+//   macroTimerFunc = () => {
+//     port.postMessage(1)
+//   }
+// } else {
+//   /* istanbul ignore next */
+//   // 都不支持的情况下，使用setTimeout
+//   macroTimerFunc = () => {
+//     setTimeout(flushCallbacks, 0)
+//   }
+// }
+
+
 export const nextTick = (function () {
   let callbacks: any[] = []
   let pending = false
